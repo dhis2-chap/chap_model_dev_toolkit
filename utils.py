3f6cee5b5f4c18cd5f02d8bbfde7b4278770e766
audunlas <61903388+audunlas@users.noreply.github.com>
2024-10-31 15:00:41 +0100

    Update utils.py
    New functions for integrating lags easily

--- utils.py
+++ utils.py
@@ -5,3 +5,15 @@ def get_df_per_location(csv_fn):
     unique_locations_list = full_df['location'].unique()
     locations = {location: full_df[full_df['location'] == location] for location in unique_locations_list}
     return locations
+
+def create_lagged_feature(df, feature, num_lags):
+    for lag in range(1, num_lags + 1):
+        df[f'{feature}_lag_{lag}'] = df[feature].shift(lag)
+
+
+def make_valid_df(df, int_remove_rows):
+    assert int_remove_rows >= 0
+    if int_remove_rows == 0:
+        df = df.fillna(0)
+    else:
+        df = df.iloc[int_remove_rows:].reset_index(drop=True)
