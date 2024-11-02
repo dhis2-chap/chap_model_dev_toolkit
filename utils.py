e04d92c36eab16c5e61df2acd9407498968eb860
geirksa <geirksa@ifi.uio.no>
2024-11-02 12:13:24 +0100

    Updated based on insights when working with minimalist lag example

--- utils.py
+++ utils.py
@@ -1,24 +1,27 @@
 import pandas as pd
 
-def get_df_per_location(csv_fn):
+def get_df_per_location(csv_fn: str) -> dict:
     full_df = pd.read_csv(csv_fn)
     unique_locations_list = full_df['location'].unique()
-    locations = {location: full_df[full_df['location'] == location] for location in unique_locations_list}
+    locations = {location: full_df[full_df['location'] == location].reset_index(drop=True) for location in unique_locations_list}
     return locations
 
-def create_lagged_feature(df, feature, num_lags):
+def get_lagged_col_name(feature, lag) -> str:
+    return f'{feature}_lag_{lag}'
+
+def create_lagged_feature(df_to_change, feature, num_lags, source_df=None):
+    if source_df is None:
+        source_df = df_to_change
+    lag_features = []
     for lag in range(1, num_lags + 1):
-        df[f'{feature}_lag_{lag}'] = df[feature].shift(lag)
+        lag_features.append( get_lagged_col_name(feature, lag) )
+        df_to_change[ lag_features[-1]] = source_df[feature].shift(lag)
+    return lag_features
 
 
-def make_valid_df(df, int_remove_rows):
-    assert int_remove_rows >= 0
-    if int_remove_rows == 0:
-        df = df.fillna(0)
-    else:
-        df = df.iloc[int_remove_rows:].reset_index(drop=True)
+def cut_top_rows(df, int_remove_rows):
+    return df.iloc[int_remove_rows:].reset_index(drop=True)
 
-
-def get_recent_data(csv_fn, periods):
-    df = pd.read_csv(csv_fn)
-    return df.tail(periods)
+def fill_top_rows_from_historic_last_rows(feature, lag, future_df, historic_df):
+    for row_index in range(lag):
+        future_df.loc[row_index, get_lagged_col_name(feature, lag) ] = historic_df[ feature ].iloc[-(lag-row_index)]
